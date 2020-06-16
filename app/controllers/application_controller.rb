65c16b6624490bcde9e7d0e972909b7f2f8952a5
alexisteh <wenhui.teh23@gmail.com>
2020-06-16 14:15:51 -0500

    multi controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,58 +1,15 @@
 
+require_relative "./puppies_controller.rb"
 
 class ApplicationController < Sinatra::Base
+  use PuppiesController 
   configure do
       set :views, 'app/views'
       set :method_override, true
-  end
+  end 
   
   get '/' do
       erb :welcome
   end
-
-  get '/puppies' do 
-    @all_pups = Puppy.all 
-    erb :index 
-  end 
-
-  post '/puppies' do 
-    puppy = Puppy.create(params)
-    redirect "/puppies/#{puppy.id}"
-  end 
-
-  delete '/puppies/:id/delete' do 
-    puppy=  find_pup(params)
-    puppy.destroy
-    redirect '/puppies'
-  end 
-
-  get '/puppies/new' do 
-    erb :new 
-  end 
-
-  get '/puppies/:id' do 
-    @puppy = find_pup(params) 
-    erb :show 
-  end 
-
-  patch '/puppies/:id' do 
-    puppy = find_pup(params) 
-    puppy.update(name: params[:name], breed: params[:breed]) 
-    redirect "/puppies/#{puppy.id}" 
-  end 
-
-  get '/puppies/:id/edit' do 
-    @puppy = find_pup(params) 
-    erb :edit 
-  end 
-
-  get '/puppies/:id/delete' do 
-    @puppy = find_pup(params) 
-    erb :delete 
-  end 
-
-  def find_pup(params)
-    Puppy.all.find(params[:id]) 
-  end 
 
 end
